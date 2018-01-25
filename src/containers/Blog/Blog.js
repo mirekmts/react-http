c27e2dbbb8cf827b70d54725ff521e5deb6ca26c
mirekmts <mirekmts@gmail.com>
2018-01-25 17:04:41 +0100

    add a post and delete method

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,18 +9,34 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0,4);
+                const updatePosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatePosts});
             });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
     }
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}/>
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>
         });
         return (
             <div>
@@ -28,7 +44,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
